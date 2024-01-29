153bb584fa6f1dec0e897b72f851e1ceb7e414e8
Aleksib <Aleksib@2000.com>
2024-01-29 21:21:35 +0800

    push commit

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
         System.out.println("pull test!");
+        System.out.println("测试!");
     }
 }
